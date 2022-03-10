46602a0c743d172d80c325bff07215a8cd21fdc2
roiaharon-pepperi <roi.a@pepperi.com>
2022-03-10 15:49:25 +0200

    remove redundant import

--- server-side/services/utilities.service.ts
+++ server-side/services/utilities.service.ts
@@ -1,6 +1,5 @@
 import { v4 as uuid } from 'uuid';
 import { CollectionsService } from './collections.service';
-import { DocumentsService } from './documents.service';
 
 export class UtilitiesService {
 
